de152d8889727afb8acfc6e09831ed397fd15076
duckling604 <mrmastercat604@gmail.com>
2025-04-13 14:43:26 -0600

    Added a variables file to properly handle use of variables in multiple files.

---
+++ restart/variables.py
@@ -0,0 +1,11 @@
+import pygame
+
+#GLOBAL VARIABLES
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+pygame.init()
+#PYGAME SPECIFIC VARIABLES
+font = pygame.font.SysFont(None,75)
+mainClock = pygame.time.Clock()
+FPS = 60
