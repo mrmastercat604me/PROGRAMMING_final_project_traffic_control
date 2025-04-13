2eb03553e6c7982788940146aae6e55b9fb884b2
duckling604 <mrmastercat604@gmail.com>
2025-04-13 01:13:26 -0600

    updated main to properly use the new button class, need to fix transparency issue.

--- restart/main.py
+++ restart/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from functions import *
+from classes import *
 
 pygame.init()
 
@@ -19,6 +20,16 @@ surface = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT), pygame.SRCALPHA)
 BackgroundImage = pygame.image.load("assets/background.png").convert()
 BackgroundImage = pygame.transform.scale(BackgroundImage,(SCREEN_WIDTH,SCREEN_HEIGHT))
 BackgroundImage_width = BackgroundImage.get_width()
+
+#create the buttons
+title_button = Button(percent_of(25,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT),percent_of(50,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT),surface,(255,255,255,0))
+start_button = Button(percent_of(25,SCREEN_WIDTH),percent_of(35,SCREEN_HEIGHT),percent_of(25,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT),surface,(200,200,200))
+options_button = Button(percent_of(25,SCREEN_WIDTH),percent_of(50,SCREEN_HEIGHT),percent_of(25,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT),surface,(200,200,200))
+start_button.centerx(percent_of(50,SCREEN_WIDTH))
+options_button.centerx(percent_of(50,SCREEN_WIDTH))
+title_button.set_text("Game Name",font,(0,0,0))
+start_button.set_text("Start",font,(0,0,0))
+options_button.set_text("Options",font,(0,0,0))
 
 #SCROLLING WINDOW VARIABLES
 scroll = 0
@@ -44,20 +55,13 @@ def main_menu():
 			scroll = 0
 		#----------------------------------------#
 		#----------DRAW-TITLE-AND-BUTTONS------#
-		mouse_x,mouse_y = pygame.mouse.get_pos()
-		secret_button = pygame.Rect(percent_of(25,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT),percent_of(50,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT))
-		start_button = pygame.Rect(percent_of(25,SCREEN_WIDTH),percent_of(35,SCREEN_HEIGHT),percent_of(25,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT))
-		options_button = pygame.Rect(percent_of(25,SCREEN_WIDTH),percent_of(50,SCREEN_HEIGHT),percent_of(25,SCREEN_WIDTH),percent_of(10,SCREEN_HEIGHT))
-		start_button.centerx = percent_of(50,SCREEN_WIDTH)
-		options_button.centerx = percent_of(50,SCREEN_WIDTH)
-		pygame.draw.rect(surface,(200,200,200),start_button)
-		pygame.draw.rect(surface,(200,200,200),options_button)
-		draw_text("Game Name",font,(0,0,0),surface,0,0,secret_button)
-		draw_text("Start",font,(0,0,0),surface,0,0,start_button)
-		draw_text("Options",font,(0,0,0),surface,0,0,options_button)
+		title_button.draw()
+		start_button.draw()
+		options_button.draw()
 		#--------------------------------------------#
 		#----------BUTTON-LOGIC--------#
-		if secret_button.collidepoint((mouse_x,mouse_y)):
+		mouse_x,mouse_y = pygame.mouse.get_pos()
+		if title_button.collidepoint((mouse_x,mouse_y)):
 			if LeftClick:
 				print("Secret")
 		if start_button.collidepoint((mouse_x,mouse_y)):
