37d3c9f39313bd2d1aa70d8b168cc87387c6e312
duckling604 <mrmastercat604@gmail.com>
2025-04-13 01:13:02 -0600

    created button class for better organisation

---
+++ restart/classes.py
@@ -0,0 +1,28 @@
+import pygame
+from functions import *
+
+class Button():
+	def __init__(self, x, y, width, height, surface,color):
+		self.x, self.y = x, y
+		self.width, self.height = width, height
+		self.rect = pygame.Rect(self.x,self.y,self.width,self.height)
+		self.surface = surface
+		self.color = color
+		self.text = False
+	
+	def centerx(self, x:int):
+		self.rect.centerx = x
+	
+	def collidepoint(self, pos):
+		return self.rect.collidepoint(pos)
+	
+	def set_text(self, text, font, color):
+		self.text = True
+		self.text = text
+		self.font = font
+		self.text_color = color
+	
+	def draw(self):
+		pygame.draw.rect(self.surface,self.color,self.rect)
+		if self.text != None:
+			draw_text(self.text ,self.font,self.text_color, self.surface, 0,0, self.rect)
