88a799d288df49a336ba2ebf15e05dfd38ffb377
duckling604 <mrmastercat604@gmail.com>
2025-04-14 20:29:48 -0600

    added note for future

--- restart/grid.py
+++ restart/grid.py
@@ -12,7 +12,9 @@ def draw_grid(surface, grid, settings)->'pygame.Surface':
 			tile = grid.grid[y][x]
 
 			create_rect = pygame.Rect(x*TILE_SIZE,y*TILE_SIZE,TILE_SIZE,TILE_SIZE)
+			
 			#handle the type logic here
+
 			pygame.draw.rect(surface,(255,255,255),create_rect)
 			#draw border around tile
 			pygame.draw.rect(surface,(0,0,0),create_rect, 1)
