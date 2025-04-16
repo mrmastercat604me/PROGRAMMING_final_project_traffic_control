a6b68d6ceb497721a4100fcacd194d917e9793e7
duckling604 <mrmastercat604@gmail.com>
2025-04-15 18:51:26 -0600

    shortened select_edge_tile() using lambda's and implemented except_tile.

--- restart/grid.py
+++ restart/grid.py
@@ -23,8 +23,7 @@ def draw_grid(surface, grid)->'pygame.Surface':
 def update_grid(grid,settings)->'Grid':
 	pass
 
-def select_edge_tile(grid,count:int=1):
-	#add optional except tile
+def select_edge_tile(grid,count:int=1,except_tile:'Tile'=None):
 	if count > 4 or count <= 0:
 		return None
 	
@@ -32,54 +31,29 @@ def select_edge_tile(grid,count:int=1):
 	#0 to x_max are valid indexes
 	y_max = grid.height -1
 	#0 to y_max are valid indexes
-	#-----TOP-EDGE-----#
-	top_y_min = 0
-	top_y_max = LOCATION_SPAWN_RANGE -1
-	random_tile_y = random.randint(top_y_min,top_y_max)
-	top_x_min = 0
-	top_x_max = x_max
-	random_tile_x = random.randint(top_x_min,top_x_max)
-	random_top_tile = grid.get_tile_within_area(x=random_tile_x, y=random_tile_y)
+	#-----DECLARE-EDGES-AND-LAMBDA-TO-CREATE-RANDOM-X,Y-VALUES-----#
+	edges = {
+		#edge name		#(generate random x value, generate random y value)
+		"top": lambda: (random.randint(0,x_max),random.randint(0,LOCATION_SPAWN_RANGE-1)),
+		"left": lambda: (random.randint(0,LOCATION_SPAWN_RANGE-1),random.randint(0,y_max)),
+		"bottom": lambda: (random.randint(0,x_max),random.randint(y_max-LOCATION_SPAWN_RANGE-1,y_max)),
+		"right": lambda: (random.randint(x_max-LOCATION_SPAWN_RANGE-1,x_max),random.randint(0,y_max))
+	}
 	#----------#
-	#-----LEFT-EDGE-----#
-	left_x_min = 0
-	left_x_max = LOCATION_SPAWN_RANGE -1
-	random_tile_x = random.randint(left_x_min,left_x_max)
-	left_y_min = 0
-	left_y_max = y_max
-	random_tile_y = random.randint(left_y_min,left_y_max)
-	random_left_tile = grid.get_tile_within_area(x=random_tile_x, y=random_tile_y)
-	#----------#
-	#-----BOTTOM-EDGE-----#
-	bottom_x_min = 0
-	bottom_x_max = x_max
-	random_tile_x = random.randint(bottom_x_min,bottom_x_max)
-	bottom_y_min = y_max - (LOCATION_SPAWN_RANGE-1)
-	bottom_y_max = y_max
-	random_tile_y = random.randint(bottom_y_min,bottom_y_max)
-	random_bottom_tile = grid.get_tile_within_area(x=random_tile_x,y=random_tile_y)
-	#----------#
-	#-----RIGHT-EDGE-----#
-	right_x_min = x_max - (LOCATION_SPAWN_RANGE-1)
-	right_x_max = x_max
-	random_tile_x = random.randint(right_x_min,right_x_max)
-	right_y_min = 0
-	right_y_max = y_max
-	random_tile_y = random.randint(right_y_min,right_y_max)
-	random_right_tile = grid.get_tile_within_area(x=random_tile_x,y=random_tile_y)
-	#----------#
-	#-----LIST-OF-RANDOM-TILES-----#
+	#-----GENERATE-LIST-OF-RANDOM-TILES-----#
 	random_tiles_list = []
-	random_tiles_list.append(random_top_tile)
-	random_tiles_list.append(random_left_tile)
-	random_tiles_list.append(random_bottom_tile)
-	random_tiles_list.append(random_right_tile)
+	for edge, coord_func in edges.items():
+		while True:
+			x, y = coord_func() #call each lambda
+			random_tile = grid.get_tile_within_area(x=x,y=y)
+			if random_tile != except_tile:
+				random_tiles_list.append(random_tile)
+				break #break out of loop to continue to next edge tile
 	#----------#
 	#-----RETURN-TILE(S)-----#
 	if count == 1:
 		return random.choice(random_tiles_list)
 	else:
 		return_list = list(random.sample(population=random_tiles_list,k=count)) #takes count many items from the population without any repeating elements
-	return return_list
-
+		return return_list
 
